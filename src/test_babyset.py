78f837ad8d3052c60e4aa07b209dc22b0d8e5d81
rvasquez3 <r.vasquez44@yahoo.com>
2017-02-15 14:52:11 -0700

    Homework complete

--- src/test_babyset.py
+++ src/test_babyset.py
@@ -29,10 +29,16 @@ class TestBabySetMethods(unittest.TestCase):
             bset.get(3)
 
     def test_remove(self):
-        pass
+        bset = BabySet([2, 4, 4])
+        self.assertEqual(bset.remove(4), 4)
+        self.assertEqual(bset.size(), 1)
+        with self.assertRaises(KeyError):
+            bset.remove(3)
 
     def test_clear(self):
-    	pass
+        bset = BabySet()
+        self.assertEqual(bset.size(), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
